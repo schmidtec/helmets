4fed37cffa4ede6a8d537cf1a7668d502df7bbfa
schmidtec <jason@schmidtec.com>
2019-03-20 16:44:18 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -64,7 +64,7 @@ export default class WearAHat {
                     break;
                 }
                 default: {
-                    HatDatabase = require('../public/1166467957212054271_space_helmets.json');
+                    HatDatabase = require('../public/1158964902488965895.json');
                     break;
                 }
             }
